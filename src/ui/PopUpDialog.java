4b6ad93de73d147067a595212ab1cb088c161169
StLuka <lukastanojkovic@gmail.com>
2018-05-15 20:09:34 +0200

    Modified PopUpDialog

--- src/ui/PopUpDialog.java
+++ src/ui/PopUpDialog.java
@@ -1,11 +1,27 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PopUpDialog {
 
-    public void winnerPopUpDialog(String winner){
-        //umesto null treba ubaciti roditelja na koji ce iskociti ovaj prozor
-        JOptionPane.showMessageDialog(null, "The winner is " + winner + "!");
+    private JPanel panel = new JPanel(new BorderLayout());
+
+    private String popUpHeader = "End of game";
+    private String[] options = {"Back to lobby" , "Exit"};
+
+    public void winnerPopUpDialog(String winner) {
+
+        JLabel text = new JLabel("The winner is " + winner + "!");
+        panel.add(text, BorderLayout.CENTER);
+
+        int selectedOption = JOptionPane.showOptionDialog(frame.getContentPane(), panel, popUpHeader, JOptionPane.YES_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, null );
+
+        if(selectedOption == 0) { //selected "Back to lobby" button
+
+        }
+        else if(selectedOption == 1){ // selected "Exit" button
+            System.exit(0);
+        }
     }
 }

1b857b0c8c30af31a0b582ba4c7d38ee0cafa85d
StLuka <lukastanojkovic@gmail.com>
2018-05-15 20:50:41 +0200

    Adding pictures

--- src/ui/PopUpDialog.java
+++ src/ui/PopUpDialog.java
@@ -15,7 +15,7 @@ public class PopUpDialog {
         JLabel text = new JLabel("The winner is " + winner + "!");
         panel.add(text, BorderLayout.CENTER);
 
-        int selectedOption = JOptionPane.showOptionDialog(frame.getContentPane(), panel, popUpHeader, JOptionPane.YES_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, null );
+        int selectedOption = JOptionPane.showOptionDialog(null, panel, popUpHeader, JOptionPane.YES_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, null );
 
         if(selectedOption == 0) { //selected "Back to lobby" button
 

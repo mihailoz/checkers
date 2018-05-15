f57fd932fbf76d111d79a0c390520c8f62d3c47d
Mihailo Zdravkovic <mihailo.zdravkovic@gmail.com>
2018-05-15 21:15:00 +0200

    Work on moving checkers

--- src/LogicTest.java
+++ src/LogicTest.java
@@ -4,23 +4,11 @@ import logic.TurnController;
 
 public class LogicTest {
     public static void main(String[] args){
-
-
-<<<<<<< HEAD
-
-
-//        TurnController controller = new TurnController("PLAYER", board);
-//        controller.makePathsForField(19);
-//        for(int i : controller.availableFields()){
-//            System.out.println(i);
-//        }
-=======
         TurnController controller = new TurnController("PLAYER", new Board());
         controller.makePathsForField(19);
         for(int i : controller.availableFields()){
             System.out.println(i);
         }
->>>>>>> 1d8c6d59cafe4f60bc6bfdc42acc83f8eb277fe2
     }
 
     static void normalBoard(Field[] board){
